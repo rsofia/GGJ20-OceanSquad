36341521ad90d5b047ef5a5263e8975f3a16a595
Sofia Brown <ruthsofiab@gmail.com>
2020-02-01 21:10:13 -0600

    random things

---
+++ Assets/_Game/Scripts/GameConsts.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConsts
+{
+    public const string playerTag = "Player";
+    public const string rockTag = "Rock";
+
+    public enum GameTags
+    {
+        Player, 
+        Rock
+    }
+
+    public static string GetTagWithEnum(GameTags t)
+    {
+        string result = "";
+
+        switch (t)
+        {
+            case GameTags.Player:
+                result = playerTag;
+                break;
+            case GameTags.Rock:
+                result = rockTag;
+                break;
+            default:
+                result = "";
+                break;
+        }
+
+        return result;
+    }
+
+    public static void LogColor(string color, string text)
+    {
+        Debug.Log("<color="+color+">" + text + "</color>");
+    }
+
+    public static void LogBold(string text)
+    {
+        Debug.Log("<b>" + text + "</b>");
+    }
+
+    public static void LogBoldColor(string color, string text)
+    {
+        Debug.Log("<b><color=" + color + ">" + text + "</color></b>");
+    }
+}
---
+++ Assets/_Game/Scripts/TriggerReward.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TriggerReward : MonoBehaviour
+{
+    [Header("Reward Settings")]
+    private bool wasActivated = false;
+    public float timeToActivate = 1.0f;
+    private string tagToDetect = "";
+    public GameConsts.GameTags gameTagToDetect;
+
+    [Header("Reward")]
+    public RewardType reward;
+    public GameObject objectToActivate;
+
+    //actions for the object to activate
+    private string triggerAction = "Open";
+    private string triggerActionClose = "Close";
+
+    //actions for self
+    private const string triggerPressed = "Pressed";
+    private const string triggerReleased = "Release";
+
+    private Animator anim;
+
+    public enum RewardType
+    {
+        _01_OpenDoor, 
+        _02_ShowLight
+    }
+
+    private void Awake()
+    {
+        tagToDetect = GameConsts.GetTagWithEnum(gameTagToDetect);
+        anim = GetComponent<Animator>();
+    }
+    private IEnumerator OnTriggerStay(Collider other)
+    {
+        GameConsts.LogBoldColor("blue", ("Colliding with " + other.tag));
+        if(other.CompareTag(tagToDetect) && !wasActivated)
+        {
+            anim.SetTrigger(triggerPressed);
+            wasActivated = true;
+            yield return new WaitForSeconds(timeToActivate);
+            OnRewardTriggred();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(wasActivated)
+        {
+            wasActivated = false;
+            anim.SetTrigger(triggerReleased);
+            objectToActivate.GetComponent<Animator>().SetTrigger(triggerActionClose);
+        }
+    }
+
+    private void OnRewardTriggred()
+    {
+        objectToActivate.GetComponent<Animator>().SetTrigger(triggerAction);
+        switch (reward)
+        {
+            case RewardType._01_OpenDoor:
+                break;
+            case RewardType._02_ShowLight:
+                break;
+            default:
+                break;
+        }
+    }
+}
