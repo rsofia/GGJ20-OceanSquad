29bc2ac195907b415fed516d3ba9c236cccd712b
Sofia Brown <ruthsofiab@gmail.com>
2020-02-02 08:55:26 -0600

    Project Settings + avances menu

---
+++ Assets/_Game/Scripts/UIMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine;
+
+public class UIMenu : MonoBehaviour
+{
+    public float timeToQuit = 2.0f;
+
+    [Header("Options")]
+    public GameObject optionsPanel;
+
+    [Header("Thank you")]
+    public Doozy.Engine.UI.UIView thankyouView;
+
+    public void OnStart()
+    {
+
+    }
+
+    public void OnClickOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void OnHideOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void OnExit()
+    {
+        thankyouView.Show();
+        StartCoroutine(Exit());
+
+    }
+
+    IEnumerator Exit()
+    {
+        yield return new WaitForSeconds(timeToQuit);
+        Application.Quit();
+    }
+}
