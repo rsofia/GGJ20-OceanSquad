135ade73be97dbe4ef3b38d36b97f7d065426747
Bait <luisbazan.dev@gmail.com>
2020-02-01 19:07:38 -0600

    Movement Changes

---
+++ Assets/_Game/Scripts/PlayerInputs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+    public float Rotation { get; private set; }
+    public float Thrust { get; private set; }
+    public Vector3 Position { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_ANDROID
+        Debug.Log(Input.touches.Length);
+        if (Input.touches.Length > 0)
+        {
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+            Debug.Log("POS: " + touchPos);
+            Position = new Vector3(touchPos.x, transform.position.y, touchPos.z);
+        }
+#elif UNITY_STANDALONE_WIN
+        Rotation = Input.GetAxis("Horizontal");
+        Thrust = Input.GetAxis("Vertical");
+#endif
+    }
+}
---
+++ Assets/_Game/Scripts/PlayerMotor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 15f;
+    [SerializeField] private float moveSpeed = 10f;
+
+    private PlayerInputs playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInputs>();
+    }
+
+    void Update()
+    {
+#if UNITY_ANDROID
+        Vector3 pos = playerInput.Position;
+        if (Vector3.Magnitude(transform.position - pos) > 0.1f)
+            transform.position = GetPosition(pos, transform.position);
+        else
+            Debug.Log("STOPPED");
+#elif UNITY_STANDALONE_WIN
+        float rotation = playerInput.Rotation;
+        float thrust = playerInput.Thrust;
+
+        transform.Rotate(Vector3.up * rotation * Time.deltaTime * turnSpeed);
+        transform.position += transform.forward * thrust * Time.deltaTime * moveSpeed;
+#endif
+    }
+
+    public Vector3 GetPosition(Vector3 _position, Vector3 _initialPos)
+    {
+        Vector3 resultPos = _initialPos;
+
+        resultPos = Vector3.Lerp(_initialPos, _position, Time.deltaTime);
+
+        return resultPos;
+    }
+}
